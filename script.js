60d606200cbe5fb261188a012710d12466a3ed47
francesco9820 <francescodiflumeri1a@gmail.com>
2023-01-10 14:36:16 +0100

    Complete assessment

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
+const checkButtonUK = document.getElementById("uk");
+const checkButtonUS = document.getElementById("us");
+let randomClick = false;
 let originalStory = `It was 94 fahrenheit outside, 
 so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. 
 Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.`
@@ -17,28 +20,58 @@ function randomValueFromArray(array){
 }
 
 
-randomize.addEventListener('click', result);
+randomize.addEventListener('click', () => {randomClick = true; result()});
+
+checkButtonUK.addEventListener('click', result);
+checkButtonUS.addEventListener('click', result);
+
+customName.addEventListener('input', result);
+
+let newString = originalStory;
 
 function result() {
+  if(randomClick){
+    newString = originalStory;
 
-  let newString = '';
+    newString = newString.replace(/:insertx:/g, randomValueFromArray(arrayNames));
+
+    newString = newString.replace(/:inserty:/g, randomValueFromArray(arrayItems));
+
+    newString = newString.replace(/:insertz:/g, randomValueFromArray(arrayItems));
+  }
 
   if(customName.value !== '') {
     const name = customName.value;
+
+    newString = newString.replace(/Bob/g, name);
+
+    story.textContent = newString;
+    story.style.visibility = 'visible';
   }
 
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+  if(checkButtonUK.checked) {
+    const weight = Math.round(300 / 2.205);
+    const temperature =  Math.round((94 - 32) * 5/9);
+
+    newString = newString.replace(/94 fahrenheit/g, temperature + ' degrees');
+    newString = newString.replace(/300 pounds/g, weight + ' kilos');
+
+    story.textContent = newString;
+
+    if(!randomClick) return;
+  }else if(checkButtonUS.checked) {
+    const weight = Math.round(300 / 2.205);
+    const temperature =  Math.round((94 - 32) * 5/9);
+
+    newString = newString.replace(`${temperature} degrees`,  '94 fahrenheit');
+    newString = newString.replace(`${weight} kilos`, '300 pounds');
+
+    story.textContent = newString;
+    
+    if(!randomClick) return;
   }
-
-  newString = originalStory.replace(/:insertx:/g, randomValueFromArray(arrayNames));
-
-  newString = newString.replace(/:inserty:/g, randomValueFromArray(arrayItems));
-
-  newString = newString.replace(/:insertz:/g, randomValueFromArray(arrayItems));
-  
 
   story.textContent = newString;
   story.style.visibility = 'visible';
+  randomClick = false;
 }
